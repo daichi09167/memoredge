ed537ca9281720df96e4b02883a1e0396d57b089
daichi09167 <hachisukadaichi@gmail.com>
2025-01-26 20:39:08 +0900

    Created the login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {CardWithForm2} from "@/components/ui/LoginForm";
+import Header from "@/components/ui/header";
+import { Center, Stack } from "@chakra-ui/react"
+
+const LoginPage: React.FC = () => {
+  return (
+    
+    <div >
+      {/* ヘッダー */}
+      <Header />
+      <Center>
+      {/* メインコンテンツ */}
+      <main >
+        <div >
+          {/* サインアップフォーム */}
+          <CardWithForm2 />
+        </div>
+      </main>
+      </Center>
+    </div>
+
+  );
+};
+
+export default LoginPage;
---
+++ src/components/ui/LoginForm.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Button, Card, Input, Stack, Box } from "@chakra-ui/react";
+import { Field } from "@/components/ui/field";
+import { Checkbox } from "@/components/ui/checkbox"
+
+export const CardWithForm2: React.FC = () => (
+  <Box
+    display="flex"
+    alignItems="center"
+    justifyContent="center"
+    minHeight="100vh"
+  >
+    <Card.Root maxW="sm" boxShadow="lg" borderRadius="md" bg="amber.50">
+      <Card.Header>
+        <Card.Title>MemorEdgeにログインしよう</Card.Title>
+        <Card.Description>
+         Fill in the form below to log in.
+        </Card.Description>
+      </Card.Header>
+      <Card.Body>
+        <Stack gap={4} width="full">
+          {/* メールアドレス */}
+          <Field label="メールアドレス">
+            <Input type="email" />
+          </Field>
+
+          {/* パスワード */}
+          <Field label="パスワード">
+            <Input type="password" />
+          </Field>
+        </Stack>
+      </Card.Body>
+      <Card.Footer display="flex" justifyContent="flex-end" gap={2}>
+        <Button 
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: '5px',
+            backgroundColor: '#FFCF56',
+            color: 'black',
+            fontWeight: 'bold',
+            padding: '5px 10px',
+            borderRadius: '5px',
+            border: 'none',
+            cursor: 'pointer',
+          }}>       
+            ログインする
+          </Button>
+      </Card.Footer>
+    </Card.Root>
+  </Box>
+);
+
+export default CardWithForm2;
