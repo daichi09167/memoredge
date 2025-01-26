5496c29c8dc5c933c8f13678888a308da0c60430
daichi09167 <hachisukadaichi@gmail.com>
2025-01-27 04:14:43 +0900

    Created a trial version of HomeHeader.tsx.

---
+++ src/components/ui/HomeHeader.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Link from "next/link"; // Link コンポーネントのインポート
+import { Avatar, AvatarGroup } from "@/components/ui/avatar";
+
+ export const HomeNavbar = () => {
+  return (
+    <nav
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: '#FFF6E8',
+        padding: '10px 20px',
+        borderBottom: '1px solid #FFECD2',
+        boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+    
+      }}
+    >
+      <Link href="/">
+      <div
+        style={{
+          fontWeight: 'bold',
+          fontSize: '24px',
+          color: '#E50000',
+        }}
+      >
+        MemorEdge
+      </div>
+      </Link>
+        {/* アバターグループ */}
+        <AvatarGroup>
+          <Avatar>
+            <img src="/path-to-avatar.jpg" alt="User Avatar" />
+          </Avatar>
+        </AvatarGroup>
+    </nav>
+  );
+};
+
+export default HomeNavbar;
